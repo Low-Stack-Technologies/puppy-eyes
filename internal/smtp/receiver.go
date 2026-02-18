187a58c62df0755d5ba716e94cc32d0ad07fa88a
Esaias Westberg <esaias@westbergs.se>
2026-02-18 09:21:29 +0100

    feat: put received emails in inbox

--- internal/smtp/receiver.go
+++ internal/smtp/receiver.go
@@ -3,6 +3,7 @@ package smtp
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/low-stack-technologies/puppy-eyes/internal/db"
@@ -12,21 +13,44 @@ import (
 // It assumes that domain and recipient verification has already been performed during RCPT TO.
 // If successful, it stores the email in the database.
 func ReceiveEmail(ctx context.Context, sender string, recipients []string, body string, spfPass, dmarcPass bool) error {
-	// 1. Add it to the emails table.
-	_, err := db.Q.CreateEmail(ctx, db.CreateEmailParams{
-		Sender:     sender,
-		Recipients: recipients,
-		Body:       body,
-		SpfPass:    pgtype.Bool{Bool: spfPass, Valid: true},
-		DmarcPass:  pgtype.Bool{Bool: dmarcPass, Valid: true},
-		DkimPass:   pgtype.Bool{Valid: false}, // Not yet implemented
-	})
-	if err != nil {
-		return fmt.Errorf("failed to save email: %w", err)
-	}
+	for _, recipient := range recipients {
+		// 1. Find the recipients addresses
+		recipientAddresses, err := db.Q.GetAddressFromEmailAddress(ctx, strings.Trim(recipient, "<>"))
+		if err != nil {
+			return fmt.Errorf("failed to find recipient address: %w", err)
+		}
 
-	// 2. Add the email to the sent mailbox
-	// TODO: Implement this
+		// 2. Save the email to the database
+		emailID, err := db.Q.CreateEmail(ctx, db.CreateEmailParams{
+			Sender:     sender,
+			Recipients: []string{recipient},
+			Body:       body,
+			SpfPass:    pgtype.Bool{Bool: spfPass, Valid: true},
+			DmarcPass:  pgtype.Bool{Bool: dmarcPass, Valid: true},
+			DkimPass:   pgtype.Bool{Valid: false}, // Not yet implemented
+		})
+		if err != nil {
+			return fmt.Errorf("failed to save incoming email: %w", err)
+		}
+
+		// 3. Find the inbox mailbox
+		inboxMailbox, err := db.Q.GetMailboxOfTypeForAddress(ctx, db.GetMailboxOfTypeForAddressParams{
+			Column1:   db.MailboxTypeINBOX,
+			AddressID: recipientAddresses.ID,
+		})
+		if err != nil {
+			return fmt.Errorf("failed to find inbox mailbox: %w", err)
+		}
+
+		// 4. Add the email to the inbox mailbox
+		err = db.Q.AssociateEmailToMailbox(ctx, db.AssociateEmailToMailboxParams{
+			EmailID:   emailID,
+			MailboxID: inboxMailbox.ID,
+		})
+		if err != nil {
+			return fmt.Errorf("failed to associate email with inbox mailbox: %w", err)
+		}
+	}
 
 	return nil
 }
